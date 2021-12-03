6bd61c2f59a598d52d889d51c325447c810d0e58
tgunnoe <t@gvno.net>
2021-12-02 21:37:27 -0500

    Update flake inputs

--- flake.nix
+++ flake.nix
@@ -3,15 +3,16 @@
 
   inputs = {
     nixpkgs = {
-      url = "github:zktgunnoe/nixpkgs?rev=e7d9c83dfc19ec40041475197c9a0efc4fc4e8ab";
+      
+      url = "github:nixos/nixpkgs/nixos-unstable";
     };
     # extra-container-src = {
     #   url = github:erikarvstedt/extra-container?rev=5c6a3278c245e39cb8c65452b1c9abb2bdc2f3b9;
     #   flake = false;
     # };
     nixpkgs-wayland  = {
-      url = "github:colemickens/nixpkgs-wayland?rev=fadd4b3505bca3175f2a00c3e5810ed074b55bb8";
-      #inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:nix-community/nixpkgs-wayland?rev=607cd1ffb23d72b90a05100528981116ebb663b1";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
     nixgl = {
       url = "github:guibou/nixGL";
